5621b7d34b5a571d89f39b55ef25b77e00db7f8c
Andres Aguirre <andres.aguirre@common.com>
2021-12-21 13:55:16 -0600

    ✅  Add visit test to index

--- src/tests/index.spec.ts
+++ src/tests/index.spec.ts
@@ -1,19 +1,26 @@
 import mongoose from "mongoose"
-import Residency from "../models/residency"
-import User from "../models/user"
-import testHelpers from "./helpers/testHelpers"
 import server from "../server"
+import testHelpers from "./helpers/testHelpers"
 import residencyTests from "./residency"
 import userTests from "./user"
 import unitTests from "./unit"
+import visitTests from "./visit"
+import Visit from "../models/visit"
+import Unit from "../models/unit"
+import User from "../models/user"
+import Residency from "../models/residency"
 
 describe("Residency Test", residencyTests)
 describe("User Tests", userTests)
 describe("Unit Tests", unitTests)
+describe("Visit Tests", visitTests)
 
 afterAll(async () => {
   await User.deleteMany()
   await Residency.deleteMany()
+  await Unit.deleteMany()
+  await Visit.deleteMany()
+
   await testHelpers.closeServer(server.httpServer)
   await mongoose.connection.close()
 })
